117a2ca7c12cb11654b5b4c45af7ac203594ad84
mitchelle lindsey <mitchellelindsey@gmail.com>
2025-05-22 17:02:11 +0100

    javascript tasks 2 to 10

---
+++ 10-factorial.js
@@ -0,0 +1,10 @@
+const num = parseInt(process.argv[2], 10);
+
+function factorial(n) {
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(num));
---
+++ 9-add.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+  return a + b;
+}
+
+const a = parseInt(process.argv[2], 10);
+const b = parseInt(process.argv[3], 10);
+
+if (isNaN(a) || isNaN(b)) {
+  console.log("Both arguments must be integers");
+} else {
+  console.log(add(a, b));
+}
