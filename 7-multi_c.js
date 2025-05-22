f0299fba678c5b20b9d903e2f589d727738af6c4
mitchelle lindsey <mitchellelindsey@gmail.com>
2025-05-22 17:07:04 +0100

    javascript corrections

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -7,5 +7,5 @@ if (isNaN(x)) {
   for (let i = 0; i < x; i++) {
     output += "C is fun\n";
   }
-  console.log(output.trim());
+  console.log(output);
 }
--- 9-add.js
+++ 9-add.js
@@ -6,7 +6,7 @@ const a = parseInt(process.argv[2], 10);
 const b = parseInt(process.argv[3], 10);
 
 if (isNaN(a) || isNaN(b)) {
-  console.log("Both arguments must be integers");
+  console.log("Please provide two integers");
 } else {
   console.log(add(a, b));
 }
