aea31edd5f6b92b0bc8c0d60acd0017700ae83f3
mitchelle lindsey <mitchellelindsey@gmail.com>
2025-05-22 17:12:14 +0100

    javascript corrections of 7

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -3,10 +3,7 @@ const x = parseInt(process.argv[2], 10);
 if (isNaN(x)) {
   console.log("Missing number of occurrences");
 } else {
-  let output = "";
   for (let i = 0; i < x; i++) {
-    output += "C is fun\n";
+    console.log("C is fun");
   }
-  // Remove trailing newline before printing
-  console.log(output.trimEnd());
 }
